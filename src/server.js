8469caa77cf3010af70d07065fcf26b42dfc4017
Oleksandr Andriichenko <panteras88smm@gmail.com>
2026-04-22 18:13:54 +0300

    test4

--- src/server.js
+++ src/server.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT ?? 3030;
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.use(authRoutes);
